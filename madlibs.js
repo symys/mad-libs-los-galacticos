592d8b36655be8b4a61b1187d4570f5d84760dce
halilibrahimcelik <halil.celik139@gmail.com>
2022-10-27 20:13:11 +0300

    rewrite test

--- madlibs.js
+++ madlibs.js
@@ -1,18 +1,18 @@
 /**
  * Complete the implementation of parseStory.
- * 
+ *
  * parseStory retrieves the story as a single string from story.txt
  * (I have written this part for you).
- * 
+ *
  * In your code, you are required (please read this carefully):
  * - to return a list of objects
  * - each object should definitely have a field, `word`
  * - each object should maybe have a field, `pos` (part of speech)
- * 
+ *
  * So for example, the return value of this for the example story.txt
  * will be an object that looks like so (note the comma! periods should
  * be handled in the same way).
- * 
+ *
  * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
  * Output: [
  *  { word: "Louis", pos: "noun" },
@@ -22,10 +22,14 @@
  *  { word: "store", pos: "noun" }
  *  { word: "," }
  *  ....
- * 
+ *
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+
+let testStr = "Hello, my name is Kevin.";
+let testRegex = /Kevin/;
+console.log(testRegex.test(testStr));
 function parseStory(rawStory) {
   // Your code here.
   return {}; // This line is currently wrong :)
@@ -34,9 +38,11 @@ function parseStory(rawStory) {
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
  * the `then` and `async` syntax for now.
- * 
+ *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
-getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-});
+getRawStory()
+  .then(parseStory)
+  .then((processedStory) => {
+    console.log(processedStory);
+  });
